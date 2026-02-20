a758dbd491622cbb169f0190aa883f2bd491cb05
Developer Shoaib <shoaibbinallahbakhsh@gmail.com>
2026-02-21 04:58:41 +0500

    refactor the code according the new backend authentication

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,24 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import { AuthProvider } from './context/AuthContext.jsx';
+import { ThemeProvider } from './context/ThemeContext.jsx';
+import { TaskProvider } from './context/TaskContext.jsx';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 createRoot(document.getElementById('root')).render(
-  <AuthProvider>
-    <App />
-  </AuthProvider>,
+  <ThemeProvider>
+    <Auth0Provider
+      domain={import.meta.env.VITE_AUTH0_DOMAIN}
+      clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <AuthProvider>
+        <TaskProvider>
+          <App />
+        </TaskProvider>
+      </AuthProvider>
+    </Auth0Provider>
+  </ThemeProvider>,
 );
